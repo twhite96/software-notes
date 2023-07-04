aad5cc7aa944fcf2537ab8eee823f35870fc6c8e
Tiffany <tiffany@tiffanywhite.dev>
2023-07-03 23:44:34 -0400

    OMG

--- src/gatsby-theme-garden/components/mdx-components/index.js
+++
@@ -1,5 +0,0 @@
-// the components provided by the theme
-export { AnchorTag as a } from "gatsby-theme-garden/src/components/mdx-components/anchor-tag"
-
-// your own component to inject into mdx
-export { Code } from "gatsby-theme-garden/src/components/mdx-components/code" // any code block with use this component
